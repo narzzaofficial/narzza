1329e494dbb4338786e23bca96d847c434af1afd
Xia Xia <700nardi@gmail.com>
2026-03-04 22:43:21 +0700

    feat: update metadata icons for improved clarity; implement admin route protection middleware

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,10 +64,7 @@ export const metadata: Metadata = {
     canonical: "https://narzza.com",
   },
   icons: {
-    icon: [
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-    ],
+     icon: "/favicon.ico",
     apple: [
       { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
     ],
